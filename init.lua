cb6a87c60b098e990bda91e4586f225c6986891d
wilkgr <wilkgr76@users.noreply.github.com>
2017-07-27 20:42:24 +1000

    just break it some more, eh?

--- init.lua
+++ init.lua
@@ -1,32 +1,45 @@
-----------------------------------------------------
--- Eggwars by wilkgr
--- Licensed under the AGPL v3
--- You MUST make any changes you make open source
--- even if you just run it on your server without publishing it
--- Supports a maximum of 8 players currently
-----------------------------------------------------
+----------------------------------------------------------------------
+-- Eggwars by wilkgr                                                --
+-- Licensed under the AGPL v3                                       --
+-- You MUST make any changes you make open source                   --
+-- even if you just run it on your server without publishing it     --
+-- Supports a maximum of 8 players currently                        --
+----------------------------------------------------------------------
 
---[[
-Shop formspec:
-
-button[0,0;10,1;upgradespeed;Upgrade speed]
-button[0,2;10,1;upgradejump;Upgrade jump]
-button[0,6;10,1;thug;Order execution (cost: 99 diamonds)]
-
-
-]]
 eggwars = {}
 eggwars.MP = minetest.get_modpath("eggwars")
 
 dofile(eggwars.MP.."/register_nodes.lua")
 
-minetest.set_mapgen_params({mgname = "singlenode"})
-local i = 1;
-local players_waiting = {};
+-------------------------
+-- SECION: Coordinates --
+-------------------------
 local waiting_area = {x=0,y=150,z=0};
 local centre = {x=0,y=100,z=0}
+eggwars.islands = {
+    {x=50,y=100,z=0},
+    {x=-50,y=100,z=0},
+    {x=0,y=100,z=50},
+    {x=0,y=100,z=-50},
+    {x=35,y=100,z=-35},
+    {x=-35,y=100,z=35},
+    {x=35,y=100,z=35},
+    {x=-35,y=100,z=-35}
+}
+
+-------------------------------
+-- Please don't modify these --
+-------------------------------
+local i = 1;
+local players_waiting = {};
 local player_i = {};
 local players_alive = {};
+local player_colours = {};
+local match_running = false;
+
+------------------------------------------------
+-- Allowed colours for nametags, chat and HUD --
+------------------------------------------------
 local allowed_colours = {
   {r = 0, g = 0, b = 255},
   {r = 0, g = 255, b = 0},
@@ -37,17 +50,6 @@ local allowed_colours = {
   {r = 255, g = 165, b = 0},
   {r = 0, g = 0, b = 0}
 }
-local player_colours = {};
-eggwars.islands = {
-  {x=50,y=100,z=0},
-  {x=-50,y=100,z=0},
-  {x=0,y=100,z=50},
-  {x=50,y=100,z=50},
-  {x=-50,y=100,z=50},
-  {x=-50,y=100,z=-50},
-  {x=0,y=100,z=-50},
-  {x=50,y=100,z=-50}
-}
 
 function StartsWith (String, Start)
     return string.sub (String, 1, string.len (Start)) == Start
@@ -57,6 +59,7 @@ function EndsWith (String, End)
     return End == '' or string.sub (String, -string.len (End)) == End
 end
 
+-- This function will remove all dropped items from the playing area
 removeDrops = function ()
   local pos  = {x=0,y=100,z=0}
   local ent  = nil
@@ -75,6 +78,7 @@ removeDrops = function ()
   end
 end
 
+-- WIP reset function restart game
 reset = function ()
   removeDrops();
   minetest.delete_area({x=-80, y=50, z=-80}, {x=80,y=150, z=80})
@@ -87,7 +91,7 @@ reset = function ()
   end
 end
 
--- Set spawnpoint to y-1, x-10, z-10
+-- Function to spawn centre island
 centrespawn = function ()
   local centre_transformed = table.copy(centre)
   centre_transformed.y = centre_transformed.y - 1
@@ -99,6 +103,7 @@ centrespawn = function ()
   minetest.place_schematic(centre_transformed, schempath.."/"..name..".mts")
 end
 
+-- Function to spawn the nth island
 islandspawn = function (n)
   --minetest.set_node(eggwars.islands[n],{name = "eggwars:egg"})
   local schem_l = table.copy(eggwars.islands[n]);
@@ -114,13 +119,12 @@ end
 
 
 minetest.register_on_dieplayer(function(player)
-  local block = minetest.get_node(player_i[player:get_player_name()]).name
-  minetest.chat_send_all(minetest.pos_to_string(player_i[player:get_player_name()]))
-  minetest.chat_send_all(minetest.get_node(player_i[player:get_player_name()]).name)
-
+  --minetest.chat_send_all(minetest.pos_to_string(player_i[player:get_player_name()]))
+  --minetest.chat_send_all(minetest.get_node(player_i[player:get_player_name()]).name)
+  -- Check if egg exists
   if minetest.get_node(player_i[player:get_player_name()]).name ~= "eggwars:egg" then
-      minetest.chat_send_all("*** "..player:get_player_name().." is " .. minetest.colorize('red','OUT'))
-    --minetest.set_player_privs(player:get_player_name(),{fly=true,fast=true,noclip=true}) --Give player fly, fast and noclip. Revokes other privs.
+    minetest.chat_send_all("*** "..player:get_player_name().." is " .. minetest.colorize('red','OUT')) -- if not, remove from game
+    minetest.set_player_privs(player:get_player_name(),{fly=true,fast=true,noclip=true}) -- and give player fly, fast and noclip.
     player:set_nametag_attributes({color = {a = 255, r = 0, g = 0, b = 0}}) --Make nametag invisible
     player:set_properties({visual_size={x=0, y=0}}) --Make player invisible
     for j=1,#players_alive do
@@ -133,15 +137,16 @@ minetest.register_on_dieplayer(function(player)
       reset();
     end
   else
-    minetest.chat_send_all("*** "..player:get_player_name().." paid Hades a visit.")
+    minetest.chat_send_all("*** "..player:get_player_name().." paid Hades a visit but was revived by their egg.")
     --player:set_player_privs({interact=true,shout=true})
   end
 end)
 
+-- Upon respawning, move players back to their island.
 minetest.register_on_respawnplayer(function(player)
   local respawn_pos = table.copy(player_i[player:get_player_name()])
   respawn_pos.y = respawn_pos.y + 2
-  minetest.after(0.1,function () player:setpos(respawn_pos) end)
+  minetest.after(0.1,function () player:setpos(respawn_pos) end) -- Wait until they have actually respawned before moving
 end)
 
 minetest.register_on_joinplayer(function(player)
@@ -151,7 +156,7 @@ minetest.register_on_joinplayer(function(player)
   minetest.set_player_privs(player_n, privs)
   player:set_nametag_attributes({color = allowed_colours[i]})
   if i == 1 then
-    centrespawn();
+    centrespawn(); -- I need to move this
     minetest.chat_send_all("Unfortunately, more than one player is required to play. Please wait for another player to join.")
   end
   if i >= 8 then
@@ -183,4 +188,5 @@ minetest.register_chatcommand("shop", {
 	end,
 })
 
+minetest.set_mapgen_params({mgname = "singlenode"})
 minetest.debug('[LOADED] Eggwars')
