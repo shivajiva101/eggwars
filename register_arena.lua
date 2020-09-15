68457b6b6640ed8e569987446a7a135911956167
shivajiva101 <shivajiva101@hotmail.com>
2020-09-15 23:14:38 +0100

    Update register_arena.lua
    Correct height of entities

--- register_arena.lua
+++ register_arena.lua
@@ -28,28 +28,28 @@ local default_arena = {
 	bot = {
 		-- spawner upgrade bot
 		offset = {
-			{x=-2, y=0, z=-4}, -- relative to island origin
-			{x=2, y=0, z=4},
-			{x=-2, y=0, z=-4},
-			{x=2, y=0, z=4},
-			{x=-4, y=0, z=2},
-			{x=-4, y=0, z=2},
-			{x=4, y=0, z=-2},
-			{x=4, y=0, z=-2}
+			{x=-2, y=2, z=-4}, -- relative to island origin
+			{x=2, y=2, z=4},
+			{x=-2, y=2, z=-4},
+			{x=2, y=2, z=4},
+			{x=-4, y=2, z=2},
+			{x=-4, y=2, z=2},
+			{x=4, y=2, z=-2},
+			{x=4, y=2, z=-2}
 		},
 		yaw = {90,270,90,270,0,0,180,180} -- facing
 	},
-  colour = {
+	colour = {
 		-- rgb colour tables
-    {r = 0, g = 0, b = 255}, -- Blue
-    {r = 0, g = 255, b = 0}, -- Green
-    {r = 255, g = 0, b = 0}, -- Red
-    {r = 108, g = 0, b = 255}, -- Purple
-    {r = 255, g = 255, b = 0}, -- Yellow
-    {r = 0, g = 255, b = 255}, -- Cyan
-    {r = 255, g = 150, b = 0}, -- Orange
-    {r = 255, g = 0, b = 255} -- Pink
-  },
+		{r = 0, g = 0, b = 255}, -- Blue
+		{r = 0, g = 255, b = 0}, -- Green
+		{r = 255, g = 0, b = 0}, -- Red
+		{r = 108, g = 0, b = 255}, -- Purple
+		{r = 255, g = 255, b = 0}, -- Yellow
+		{r = 0, g = 255, b = 255}, -- Cyan
+		{r = 255, g = 150, b = 0}, -- Orange
+		{r = 255, g = 0, b = 255} -- Pink
+},
 	cs = {
 		-- colour strings used by mod
 		{"Blue", "#0000FFBF", "blue", "0x0000FF"},
@@ -62,16 +62,16 @@ local default_arena = {
 		{"Pink", "#FF00FFBF", "pink", "0xFF00FF"}
 	},
 	description = 'default arena by shivajiva',
-  egg_offset = {
-    {x=-5, y=0, z=0}, -- relative to island origin
-    {x=5, y=0, z=0},
-    {x=-5, y=0, z=0},
-    {x=-5, y=0, z=0},
-    {x=-5, y=0, z=0},
-    {x=-5, y=0, z=0},
-    {x=-5, y=0, z=0},
-    {x=-5, y=0, z=0}
-  },
+	egg_offset = {
+		{x=-5, y=0, z=0}, -- relative to island origin
+		{x=5, y=0, z=0},
+		{x=-5, y=0, z=0},
+		{x=-5, y=0, z=0},
+		{x=-5, y=0, z=0},
+		{x=-5, y=0, z=0},
+		{x=-5, y=0, z=0},
+		{x=-5, y=0, z=0}
+	},
 	exempt_nodes = {
 		-- exempt nodes you want breakable within the world
 		-- partial strings allowed
@@ -79,35 +79,35 @@ local default_arena = {
 		'wool:', -- bridging block
 		'default:obsidian_block' -- requires pick
 	},
-  hub = {
+	hub = {
 		-- arena hub def
-    offset = {x=-30, y=-18, z=-30}, -- pos1 for place
+		offset = {x=-30, y=-18, z=-30}, -- pos1 for place
 		schem = 'hub_1.mts'
-  },
-  island = {
+	},
+	island = {
 		-- Confined to 90 degree rotations the current schema
-	  -- places 2 islands per side of a square, giving us 8
-	  -- islands max per instance. Each island is displaced
-	  -- equally from the arena centre position using the
-	  -- island vector table
-    {x=85, y=0, z=-40}, -- BL
-    {x=-85, y=0, z=-40}, -- TL
-    {x=85, y=0, z=40}, -- BR
-    {x=-85, y=0, z=40}, -- TR
-    {x=40, y=0, z=-85}, -- LB
-    {x=-40, y=0, z=-85}, -- LT
-    {x=40, y=0, z=85}, -- RB
-    {x=-40, y=0, z=85}, -- RT
-    offset = {x=-11, y=-20, z=-10},
-		rotate = {"0", "180", "0", "180", "90", "90", "270", "270"},
-		schem = 'island_1.mts'
-  },
+		-- places 2 islands per side of a square, giving us 8
+		-- islands max per instance. Each island is displaced
+		-- equally from the arena centre position using the
+		-- island vector table
+		{x=85, y=0, z=-40}, -- BL
+		{x=-85, y=0, z=-40}, -- TL
+		{x=85, y=0, z=40}, -- BR
+		{x=-85, y=0, z=40}, -- TR
+		{x=40, y=0, z=-85}, -- LB
+		{x=-40, y=0, z=-85}, -- LT
+		{x=40, y=0, z=85}, -- RB
+		{x=-40, y=0, z=85}, -- RT
+		offset = {x=-11, y=-20, z=-10},
+			rotate = {"0", "180", "0", "180", "90", "90", "270", "270"},
+			schem = 'island_1.mts'
+	},
 	name = 'default', -- arena name
-  region = {
+	region = {
 		-- arena region relative to origin
-    p1 = {x=-120, y=-30, z=-120},
-    p2 = {x=120, y=100, z=120}
-  },
+		p1 = {x=-120, y=-30, z=-120},
+		p2 = {x=120, y=100, z=120}
+	},
 	register_nodes = {
 		-- add any arena specific custom nodes here, generally
 		-- these are the nodes the player can use in the arenas
@@ -135,9 +135,9 @@ local default_arena = {
 		},
 		schem = 'island_small_1.mts'
 	},
-  spawners = {
+	spawners = {
 		-- centre
-    diamond = {
+		diamond = {
 			{x=9, y=4, z=-9}, -- relative to arena origin
 			{x=-9, y=4, z=9},
 			{x=-9, y=4, z=-9},
@@ -145,31 +145,31 @@ local default_arena = {
 			rate = 30
 		},
 		-- player island
-    gold = {
-      {x=-3, y=-1, z=-4}, -- relative to island origin
-      {x=3, y=-1, z=4},
-      {x=-3, y=-1, z=-4},
-      {x=3, y=-1, z=4},
-      {x=-4, y=-1, z=3},
-      {x=-4, y=-1, z=3},
-      {x=4, y=-1, z=-3},
-      {x=4, y=-1, z=-3},
+		gold = {
+			{x=-3, y=-1, z=-4}, -- relative to island origin
+			{x=3, y=-1, z=4},
+			{x=-3, y=-1, z=-4},
+			{x=3, y=-1, z=4},
+			{x=-4, y=-1, z=3},
+			{x=-4, y=-1, z=3},
+			{x=4, y=-1, z=-3},
+			{x=4, y=-1, z=-3},
 			rate = 5 -- starting rate in seconds
-    },
+		},
 		-- satellites
-    ruby = {
+		ruby = {
 			{x=65, y=0.5, z=0}, -- relative to arena origin
 			{x=0, y=0.5, z=65},
 			{x=-65, y=0.5, z=0},
 			{x=0, y=0.5, z=-65},
 			rate = 15
 		},
-  },
+    },
 	timer = {
 		max_time = 1800, -- seconds
 		suddendeath = 1200 -- seconds
 	},
-  trader = {
+	trader = {
 		names = {
 			"Johann",
 			"Celery55",
@@ -181,14 +181,14 @@ local default_arena = {
 			"Bom Shiva"
 		},
 		offset = {
-			{x=2, y=0.5, z=0}, -- relative to island origin
-	    {x=-2, y=0.5, z=0},
-	    {x=2, y=0.5, z=0},
-	    {x=-2, y=0.5, z=0},
-	    {x=0, y=0.5, z=2},
-	    {x=0, y=0.5, z=2},
-	    {x=0, y=0.5, z=-2},
-	    {x=0, y=0.5, z=-2}
+			{x=2, y=2.5, z=0}, -- relative to island origin
+			{x=-2, y=2.5, z=0},
+			{x=2, y=2.5, z=0},
+			{x=-2, y=2.5, z=0},
+			{x=0, y=2.5, z=2},
+			{x=0, y=2.5, z=2},
+			{x=0, y=2.5, z=-2},
+			{x=0, y=2.5, z=-2}
 		},
 		yaw = {90,270,90,270,0,0,180,180} -- facing
   },
