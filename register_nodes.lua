37c1eb82a463c3347398a3f09e6c0487aae8f747
MinetestSam <samarth0036@gmail.com>
2020-09-15 19:41:25 +0530

    Add HUD when egg is destroyed

--- register_nodes.lua
+++ register_nodes.lua
@@ -84,6 +84,7 @@ for i, v in ipairs(eggwars.arena[1].cs) do
 			match.player[attacker].eggs = eggs
 			msg = victim .. "'s egg was destroyed by " .. attacker
 			eggwars.chat_send_match(key, msg)
+			eggwars.add_tmp_image(victim, 'egg_destroyed.png', 5)
 			eggwars.update_hud(key, match.player[victim].id)
 			eggwars.match[key] = match
 		end,
