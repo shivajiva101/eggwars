049dd919e8120120376bd850412335ccee921e38
MinetestSam <samarth0036@gmail.com>
2020-09-18 19:52:48 +0530

    Increase sprint speed to 1.3 and add change in fov when sprinting

--- sprint.lua
+++ sprint.lua
@@ -7,10 +7,17 @@ minetest.register_globalstep(function(dtime)
 		local control = player:get_player_control()
 		local sprint = control.aux1 and control.up
 		if sprint and players[name] then
-			player:set_physics_override({speed = 1.2})
+			player:set_physics_override({speed = 1.3})
+			player:set_fov(1.2, true, 0.1)
 		else
 			player:set_physics_override({speed = 1})
+			player:set_fov(1, true, 0.1)
 			players[name] = nil
 		end
 	end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	local name = player:get_player_name()
+	players[name] = nil
 end)
